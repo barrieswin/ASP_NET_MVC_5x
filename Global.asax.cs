56b79757b1732745cad63e584bab950757f70d54
barrieswin <102302008+barrieswin@users.noreply.github.com>
2022-05-10 09:29:22 +0800

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace TheOceanWeb
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+
+        //管理者驗證
+        public MvcApplication()
+        {
+            AuthorizeRequest += new EventHandler(Application_AuthenticateRequest);
+        }
+        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie == null || authCookie.Value == "")
+            {
+                return;
+            }
+            FormsAuthenticationTicket authTicket = null;
+            try
+            {
+                authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+            }
+            catch
+            {
+                return;
+            }
+            string[] roles = authTicket.UserData.Split(new char[] { '，' });
+            if (Context.User != null)
+            {
+                Context.User = new System.Security.Principal.GenericPrincipal(Context.User.Identity, roles);
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
